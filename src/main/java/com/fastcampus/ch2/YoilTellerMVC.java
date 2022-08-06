c110660a6956faa3bff5433b93d9a6893453ade8
Jongkwan Choi <doemqjffns97@naver.com>
2022-08-06 22:40:30 +0900

    2022.08.06

--- src/main/java/com/fastcampus/ch2/YoilTellerMVC.java
+++ src/main/java/com/fastcampus/ch2/YoilTellerMVC.java
@@ -1,45 +1,46 @@
 package com.fastcampus.ch2;
 
-import java.io.IOException;
 import java.util.Calendar;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class YoilTellerMVC {
-    @RequestMapping("/getYoilMVC") // http://localhost:8080/ch2/getYoil?year=2021&month=10&day=1
-    	public String main( int year, int month,int day, Model model) throws IOException {
+    @RequestMapping("/getYoilMVC") // http://localhost/ch2/getYoilMVC
+    public String main(int year, int month, int day, Model model) {
+ 
         // 1. 유효성 검사
-    	if(!isValid(year, month, day)) {
-    		return "yoilError";
-    	}
-        // 2. 요청 계산
-       char yoil= getYoil(year, month, day);
+    	if(!isValid(year, month, day)) 
+    		return "yoilError";  // 유효하지 않으면, /WEB-INF/views/yoilError.jsp로 이동
+    	
+        // 2. 처리
+    	char yoil = getYoil(year, month, day);
+
+        // 3. Model에 작업 결과 저장
+        model.addAttribute("year", year);
+        model.addAttribute("month", month);
+        model.addAttribute("day", day);
+        model.addAttribute("yoil", yoil);
         
-       //3. 계산한 결과를 model에 저장
-       model.addAttribute("year",year);
-       model.addAttribute("month",month);
-       model.addAttribute("day",day);
-       model.addAttribute("yoil",yoil);
-       
-        
-       
-        return "yoil";// jsp 파일  WEB-INF/views/yoil.jsp
-        
+        // 4. 작업 결과를 보여줄 View의 이름을 반환
+        return "yoil"; // /WEB-INF/views/yoil.jsp
     }
-
-	private boolean isValid(int year, int month, int day) {
-		// TODO Auto-generated method stub
-		return true;
-	}
-
-	private char getYoil(int year, int month, int day) {
-		Calendar cal = Calendar.getInstance();
+    
+    private char getYoil(int year, int month, int day) {
+        Calendar cal = Calendar.getInstance();
         cal.set(year, month - 1, day);
 
         int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
-       return " 일월화수목금토".charAt(dayOfWeek);
-	}
+        return " 일월화수목금토".charAt(dayOfWeek);
+    }
+    
+    private boolean isValid(int year, int month, int day) {    
+    	if(year==-1 || month==-1 || day==-1) 
+    		return false;
+    	
+    	return (1<=month && month<=12) && (1<=day && day<=31); // 간단히 체크 
+    }
 }
